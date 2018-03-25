c510550c21d16050366d4a1aed6198dbbe549db0
James McNicholas <jmcnicholas.62743@farnborough.ac.uk>
2018-03-25 17:37:20 +0100

    Final features and polishing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -19,6 +20,7 @@ public class Main extends Application {
         stage.setTitle("Paper Progress");
         stage.setResizable(false);
         stage.setScene(scene);
+        stage.getIcons().add(new Image("logo.png"));
 
         stage.show();
     }
